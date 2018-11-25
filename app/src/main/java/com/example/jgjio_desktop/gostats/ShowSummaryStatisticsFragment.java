b03094b7028a0bd99727a84927754967e07502ff
John Jackson <johngjackson0@gmail.com>
2018-11-25 15:03:26 -0500

    redesign 1 var stats fragment + add clipboard function

--- app/src/main/java/com/example/jgjio_desktop/gostats/ShowSummaryStatisticsFragment.java
+++ app/src/main/java/com/example/jgjio_desktop/gostats/ShowSummaryStatisticsFragment.java
@@ -2,22 +2,35 @@ package com.example.jgjio_desktop.gostats;
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 
 import java.util.List;
 
-public class ShowSummaryStatisticsFragment extends Fragment {
+import static android.support.v4.content.ContextCompat.getColor;
+
+public class ShowSummaryStatisticsFragment extends Fragment implements View.OnClickListener {
     private int mListID;
     public static final String EXTRA_LIST_ID = "com.example.jgjio_desktop.gostats.extra.LIST_ID";
+
+    private Button mCopyToClipboard;
+    private String toText = "";
 
     private ShowSummaryStatisticsViewModel mViewModel;
 
@@ -35,6 +48,9 @@ public class ShowSummaryStatisticsFragment extends Fragment {
         mListID = getArguments().getInt(EXTRA_LIST_ID);
         View rootView = inflater.inflate(R.layout.show_summary_statstics_fragment, container, false);
         fillOneVariableStatistics(rootView);
+
+        mCopyToClipboard = rootView.findViewById(R.id.copy_to_clipboard_button);
+        mCopyToClipboard.setOnClickListener(this);
 
         return rootView;
     }
@@ -84,12 +100,28 @@ public class ShowSummaryStatisticsFragment extends Fragment {
                         descStat.addValue(v.getValue());
                     }
                 }
+                toText="1 variable Stats : Go! Statistics\n" +
+                        getString(R.string.summary_mean_label) + " " + Double.toString(descStat.getMean()) + "\n" +
+                        getString(R.string.summary_sigma_label) + "  " + Double.toString(descStat.getSum()) + "\n" +
+                        getString(R.string.summary_sigma_squared_label) + "  " + Double.toString(descStat.getSumsq()) + "\n" +
+                        getString(R.string.summary_sample_standard_deviation_label) + "  " + Double.toString(descStat.getStandardDeviation()) + "\n" +
+                        getString(R.string.summary_standard_deviation_label) + "  " + Double.toString(Math.sqrt(descStat.getPopulationVariance())) + "\n" +
+                        getString(R.string.summary_number_of_items_label) + "  " + Long.toString(descStat.getN()) + "\n" +
+                        getString(R.string.summary_min_label) + "  " + Double.toString(descStat.getMin()) + "\n" +
+                        getString(R.string.summary_quartile_one_label) + "  " + Double.toString(descStat.getPercentile(25))+ "\n" +
+                        getString(R.string.summary_median_label) + "  " + Double.toString(descStat.getPercentile(50)) + "\n" +
+                        getString(R.string.summary_quartile_three_label) + "  " + Double.toString(descStat.getPercentile(75)) + "\n" +
+                        getString(R.string.summary_max_label) + "  " + Double.toString(descStat.getMax()) + "\n";
+
+
                 mean.setText(Double.toString(descStat.getMean()));
                 sigma.setText(Double.toString(descStat.getSum()));
                 sigmaSquared.setText(Double.toString(descStat.getSumsq()));
                 sampleStandardDeviation.setText(Double.toString(descStat.getStandardDeviation()));
                 standardDeviation.setText(Double.toString(Math.sqrt(descStat.getPopulationVariance())));
+
                 numberItems.setText(Long.toString(descStat.getN()));
+
                 min.setText(Double.toString(descStat.getMin()));
                 quartileOne.setText(Double.toString(descStat.getPercentile(25)));
                 median.setText(Double.toString(descStat.getPercentile(50)));
@@ -101,4 +133,30 @@ public class ShowSummaryStatisticsFragment extends Fragment {
     }
 
 
+    private void copyToClipboard(String copyText) {
+        ClipboardManager clipboard = (ClipboardManager) getActivity().getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("1-var-stats", copyText);
+        clipboard.setPrimaryClip(clip);
+    }
+
+    @Override
+    public void onClick(View view) {
+        copyToClipboard(toText);
+        showCopyToClipboardMessage();
+    }
+
+    private void showCopyToClipboardMessage() {
+        Toast toast = Toast.makeText(getActivity(),
+                "Copied To Clipboard",
+                Toast.LENGTH_SHORT);
+
+        View view = toast.getView();
+        TextView text = view.findViewById(R.id.message);
+
+        int color = ContextCompat.getColor(getContext(), R.color.colorPrimary);
+
+        view.getBackground().setColorFilter(color, PorterDuff.Mode.SRC_IN);
+        toast.show();
+
+    }
 }
