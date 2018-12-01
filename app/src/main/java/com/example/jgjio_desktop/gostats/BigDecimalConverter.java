17d64e3f3c1cdf8f3a2326fc69789be9f5d457e4
John Jackson <johngjackson0@gmail.com>
2018-12-01 18:19:14 -0500

    fix typeConverter for BigDecimal

--- app/src/main/java/com/example/jgjio_desktop/gostats/BigDecimalConverter.java
+++ app/src/main/java/com/example/jgjio_desktop/gostats/BigDecimalConverter.java
@@ -6,16 +6,16 @@ import java.math.BigDecimal;
 
 public class BigDecimalConverter {
     @TypeConverter
-    public BigDecimal fromLong(Long value) {
-        return value == null ? null : new BigDecimal(value).divide(new BigDecimal(100));
+    public BigDecimal fromString(String value) {
+        return value == null ? null : new BigDecimal(value);
     }
 
     @TypeConverter
-    public Long toLong(BigDecimal bigDecimal) {
+    public String toString(BigDecimal bigDecimal) {
         if (bigDecimal == null) {
             return null;
         } else {
-            return bigDecimal.multiply(new BigDecimal(100)).longValue();
+            return bigDecimal.toString();
         }
     }
 
